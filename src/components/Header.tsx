ca6e1d17b7edac7df0642a32c5efb79777e02b55
KoridzeG <giorgiqori@yahoo.com>
2022-11-26 02:37:36 +0400

    feat: header UI

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,8 @@
-import React from 'react'
-import { Box, Typography, IconButton, Icon } from "@mui/material"
+import React, { useState } from 'react'
+import { Box, Typography, IconButton, Icon, Drawer, Divider } from "@mui/material"
+import CloseIcon from '@mui/icons-material/Close';
+import SettingsIcon from '@mui/icons-material/Settings';
+import NightlightIcon from '@mui/icons-material/Nightlight';
 import SportsSoccerIcon from '@mui/icons-material/SportsSoccer';
 import SearchIcon from '@mui/icons-material/Search';
 import MenuIcon from '@mui/icons-material/Menu';
@@ -8,7 +11,14 @@ import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
     const navigate = useNavigate();
+    const [isOpen, setIsOpen] = useState<boolean>(false)
 
+    const toggleDrawer = (isOpen: boolean) => (event: any) => {
+        if (event.type === "keydown" && (event.key === "Tab" || event.key === "Shift")) {
+            return;
+        }
+        setIsOpen(isOpen)
+    }
 
 
     return (
@@ -28,17 +38,61 @@ const Header = () => {
                 zIndex="22"
                 borderBottom="1px solid #7e6868"
             >
-                <Box>
+                <Box display="flex" gap="0.5rem" width="100%">
                     <IconButton onClick={() => {
                         navigate("/")
                     }}>
                         <SportsSoccerIcon />
                     </IconButton>
-                    <IconButton>
+                    <IconButton
+                        edge="start"
+                        aria-label="open drawer"
+                        onClick={toggleDrawer(true)}
+                        sx={{
+                            mr: 2,
+                            display: {
+                                xs: 'block',
+                            }
+                        }}
+                    >
                         <MenuIcon />
                     </IconButton>
+                    {/*The outside of the drawer */}
+
+                    <Drawer
+                        anchor="left"
+                        open={isOpen}
+                        onClose={toggleDrawer(false)}
+
+                    >
+                        {/* Inside of the drawer */}
+                        <Box sx={{
+                            p: 2,
+                            height: 1,
+                            backgroundColor: "#523939"
+                        }}>
+                            <IconButton sx={{ mb: 2 }}>
+                                <CloseIcon onClick={toggleDrawer(false)} />
+                            </IconButton>
+                            <Divider sx={{ mb: 2 }} />
+                            <Box sx={{ mb: 2 }}>
+                                <IconButton>
+                                    <SettingsIcon />
+                                </IconButton>
+
+                            </Box>
+                            <Divider sx={{ mb: 2 }} />
+                            <Box sx={{ mb: 2 }}>
+                                <IconButton>
+                                    <NightlightIcon />
+                                </IconButton>
+                            </Box>
+
+                        </Box>
+
+                    </Drawer>
                 </Box>
-                <Box>
+                <Box display="flex" width="100%" justifyContent="flex-end">
 
                     <IconButton>
                         <SearchIcon />
